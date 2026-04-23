5b17b4305ad8687d5e230fa3f789d86416008fee
janidurukshan1 <janidurukshan300@gmail.com>
2026-04-23 11:45:49 +0530

    Add users object with corresponding user IDs

---
+++ users.js
@@ -0,0 +1,11 @@
+export const users = {
+  "Janidu Rukshan": "839274",
+  "Saditha": "562193",
+  "Sasmitha": "741026",
+  "Dulina": "905317",
+  "Inura": "228491",
+  "Vinupa": "667204",
+  "Thamidu": "310982",
+  "Chamidu": "884120",
+  "Hashan Devinda": "459673"
+};
