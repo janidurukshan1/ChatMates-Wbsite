dd2495ded63b68f259a7d30bba84957fd1bedf4d
janidurukshan1 <janidurukshan300@gmail.com>
2026-04-23 19:43:10 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,19 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDUvwNFqMNHO_REKwHt0uYL4xOG2TMK0BU",
   authDomain: "chatmates-e2c70.firebaseapp.com",
-  projectId: "chatmates-e2c70",
-  storageBucket: "chatmates-e2c70.firebasestorage.app",
-  messagingSenderId: "822767312755",
-  appId: "1:822767312755:web:9bf754e97657c1c992ef06",
-  measurementId: "G-5D6539RR2K"
+  databaseURL: "https://console.firebase.google.com/project/chatmates-e2c70/database/chatmates-e2c70-default-rtdb/data/~2F?fb_gclid=CjwKCAjwhqfPBhBWEiwAZo196tmBuXrWxePhLiGiPCYG6XEqE4u5ikpzBa8oxYrrYbhS1nsyfi_4ORoCicwQAvD_BwE&fb_utm_campaign=Cloud-SS-DR-Firebase-FY26-global-gsem-1713590&fb_utm_content=text-ad&fb_utm_medium=cpc&fb_utm_source=google&fb_utm_term=KW_firebase%20console",
+  projectId: "chatmates-e2c70"
 };
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getDatabase(app);
+export const auth = getAuth(app);
+
+// export DB functions also
+export { ref, push, onValue };
