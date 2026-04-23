8f1c2783944bc7daeb3da146ac99af59e80d3cec
janidurukshan1 <janidurukshan300@gmail.com>
2026-04-23 20:47:26 +0530

    Update Firebase database URL and formatting

--- firebase.js
+++ firebase.js
@@ -5,7 +5,7 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 const firebaseConfig = {
   apiKey: "AIzaSyDUvwNFqMNHO_REKwHt0uYL4xOG2TMK0BU",
   authDomain: "chatmates-e2c70.firebaseapp.com",
-  databaseURL: "https://console.firebase.google.com/project/chatmates-e2c70/database/chatmates-e2c70-default-rtdb/data/~2F?fb_gclid=CjwKCAjwhqfPBhBWEiwAZo196tmBuXrWxePhLiGiPCYG6XEqE4u5ikpzBa8oxYrrYbhS1nsyfi_4ORoCicwQAvD_BwE&fb_utm_campaign=Cloud-SS-DR-Firebase-FY26-global-gsem-1713590&fb_utm_content=text-ad&fb_utm_medium=cpc&fb_utm_source=google&fb_utm_term=KW_firebase%20console",
+  databaseURL: "https://chatmates-e2c70-default-rtdb.firebaseio.com/",
   projectId: "chatmates-e2c70"
 };
 
